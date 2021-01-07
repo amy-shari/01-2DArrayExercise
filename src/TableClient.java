651cc98e23aeabcd41237bb758fcf2ef50b0fd54
amann <amann@localhost>
2021-01-07 10:14:34 -0600

    I created a client template. I am getting rid of my original template.

---
+++ src/TableClient.java
@@ -0,0 +1,62 @@
+/***********************************************************************
+* Programmer:	Amy Mann
+* Class:	CS40S
+*
+* Assignment:	01 2D Array Exercise
+*
+* Description:	client code
+*
+*
+*  Input:	tableData.txt
+*
+*  Output:	prints table, prints table in reverse order, prints rows
+*               & columns in reverse order, prints row by row, and column
+*               by column, prints sum & average of each row and of each
+*               column, prints sum of any two table elements
+************************************************************************/
+
+import javax.swing.*;
+import java.text.DecimalFormat;
+
+public class TableClient/*CHANGE THIS TO THE NAME OF THE FILE*/
+{  // begin class
+	public static void main(String args[])
+	{  // begin main
+	// ***** declaration of constants *****
+	
+	// ***** declaration of variables *****
+	
+	// ***** create objects *****
+		
+		
+	// ***** create input stream *****
+	
+		//ConsoleReader console = new ConsoleReader(System.in);
+		
+	// ***** Print Banner *****
+	
+		System.out.println("**********************************");
+		System.out.println("NAME:        Your Name Here");
+		System.out.println("Class:       CS30S");
+		System.out.println("Assignment:  Ax Qy");
+		System.out.println("**********************************");
+		
+	// ***** get input *****
+	
+		// all input is gathered in this section
+		// remember to put ConsoleReader.class into the
+		// same folder.
+	
+	// ***** processing *****
+	
+		
+	// ***** output *****
+	
+		// all formatted ouput is printed in this section
+
+	// ***** closing message *****
+	
+		System.out.println("end of processing");
+	
+	}  // end main	
+}  // end class
