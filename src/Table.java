f519a9ac1050df1560b1831a1795a9d7055a3fe4
amann <amann@localhost>
2021-01-07 09:47:46 -0600

    I created a table class.

---
+++ src/Table.java
@@ -0,0 +1,34 @@
+/* **********************************************************
+ * Programmer:	Amy Mann
+ * Class:		CS40S
+ * 
+ * Assignment:	01 2D Array Exercise
+ *
+ * Description:	describe the class you are creating
+ *
+ * *************************************************************
+ */
+ 
+ // import files here as needed
+ 
+ 
+ public class Table
+ {  // begin class
+ 	
+ 	// *********** class constants **********
+ 	
+ 	
+ 	// ********** instance variable **********
+ 	
+ 	// ********** constructors ***********
+ 	
+ 	// ********** accessors / getters **********
+ 	
+     //*****************************************************
+    // Purpose: get the id number of the circle
+    // Interface: IN: none
+    // Returns: current state of property id (id number)
+    // *****************************************************          	
+ 	// ********** mutators / setters **********
+ 
+ }  // end class
