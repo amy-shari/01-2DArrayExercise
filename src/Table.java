f8b131babad0c2d445a012cd3f41eeaf17d6012e
amann <amann@localhost>
2021-01-07 09:54:47 -0600

    I added constructors.

--- src/Table.java
+++ src/Table.java
@@ -20,8 +20,34 @@
  	
  	// ********** instance variable **********
  	
+            int rows = 0;       // number of rows
+            int colomns = 0;    // number of colomns
+            
  	// ********** constructors ***********
  	
+            /*****************************************
+            * Description: default constructor
+            * 
+            * ****************************************/
+                public Table(){
+                    this.rows = 0;        // sets rows to zero
+                    this.colomns = 0;     // sets colomns to zero
+                }  // end default constructor
+            
+            /*****************************************
+            * Description: initialized constructor
+            * 
+            * Interface:
+            * 
+            * @param        rows : int, number of rows
+            * @param        colomns : int, number of colomns
+            * 
+            * ****************************************/
+                public Table(int rows, int colomns){
+                    this.rows = rows;
+                    this.colomns = colomns;
+                } // end initialized constructor
+     
  	// ********** accessors / getters **********
  	
      //*****************************************************
